e8848c851571e9f86ad9d10010b87d928e110e1a
JGalera22 <galera221412@gmail.com>
2021-01-18 09:10:09 +0100

    configuración básica 2º paso

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("¡Hola Mundo!");
+  });
+
+  app.listen(process.env.PORT, () =>
+  console.log(
+    `¡Aplicación de ejemplo escuchando en el puerto ${process.env.PORT}!`
+  )
+);
+
+
 console.log("Hola 👋 Node.js.");
 console.log(process.env.MI_PASSWORD);
-console.log("Odio esto")
+console.log(process.env.PORT);
+console.log("Odio esto");
